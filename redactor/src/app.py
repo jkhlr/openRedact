be53256b5648b8b5e846185301ef58fdac431874
jkhlr <13289385+jkhlr@users.noreply.github.com>
2019-12-02 21:35:22 -0800

    add ML module

--- redactor/src/app.py
+++ redactor/src/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
 
+from .predict import predict_redaction_labels
+
 app = Flask(__name__)
 
 
@@ -11,15 +13,8 @@ def redact():
         return {'error': "Missing key in JSON body: 'text'"}
 
     words = request.json['text'].split(' ')
-    h0, h1 = classify(words)
+    h0, h1 = predict_redaction_labels(words)
     return {'text': words, 'H0': h0, 'H1': h1}
-
-
-def classify(words):
-    # TODO: replace dummy code with actual classification logic
-    h0 = [int(bool(i % 2)) for i in range(len(words))]
-    h1 = [int(not bool(i % 2)) for i in range(len(words))]
-    return h0, h1
 
 
 if __name__ == '__main__':
---
+++ redactor/src/predict.py
@@ -0,0 +1,55 @@
+import string
+
+import spacy
+import pandas as pd
+
+
+def predict_redaction_labels(words):
+    # TODO: rewritten so that the last sentence without a period is not dropped
+    myData = [
+        sentence.strip()
+        for sentence in ' '.join(words).split('.')
+        if sentence.strip()
+    ]
+    data = pd.DataFrame(myData)
+    print(data)
+    data.columns=['Sentence']
+
+    model_dir = "model"
+    nlp2 = spacy.load(model_dir)
+    nlp = spacy.load("en_core_web_sm")
+
+    table = str.maketrans(dict.fromkeys(string.punctuation))
+    h0, h1 = [],  []
+    for index, row in data.iterrows():
+        sent_mymodel = nlp2(row['Sentence'])
+        sent_inbuilt = nlp(row['Sentence'])
+        entities = []
+        for ent in sent_inbuilt.ents:
+            if (ent.label_ == 'PERSON'):
+                entities.append([ent, 'H0'])
+            elif (ent.label_ == 'NORP'):
+                entities.append([ent, 'H1'])
+        for ent in sent_mymodel.ents:
+            entities.append([ent, ent.label_])
+        # TODO: strip() to avoid empty string for a sentence that ends with a space
+        words = row['Sentence'].strip().split(" ")
+        for word in words:
+            entity_name = None
+            for entity in entities:
+                # TODO: because of check with 'in', the word 'a' is almost always redacted
+                if (word.translate(table) in str(entity[0])):
+                    entity_name = str(entity[1])
+                    break
+
+            if (entity_name != None):
+                if (entity_name == 'H0'):
+                    h0.append(1)
+                else:
+                    h0.append(0)
+                h1.append(1)
+            else:
+                h0.append(0)
+                h1.append(0)
+
+    return h0, h1
